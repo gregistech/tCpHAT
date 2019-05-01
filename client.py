3aa537a56767f4e5b41b562f921be1c355c9588a
thegergo02 <thegergo02@tutanota.com>
2019-05-01 12:17:59 +0200

    prototype: some test code #2

---
+++ client.py
@@ -0,0 +1,11 @@
+import socket
+
+connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connection.connect(("127.0.0.1", 5555))
+while True:
+    msg = input("Message: ")
+    connection.send(msg.encode("utf-8"))
+    data = connection.recv(2048)
+    if not data: break
+    data = data.decode("utf-8")
+    print(data)
--- server.py
+++ server.py
@@ -1,3 +1,33 @@
 import socket
 
+def listen():
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    connection.bind(('0.0.0.0', 5555))
+    connection.listen(10)
+    while True:
+        current_connection, address = connection.accept()
+        while True:
+            data = current_connection.recv(2048)
+            data = data.decode("utf-8")
 
+            if data == "quit":
+                current_connection.shutdown(1)
+                current_connection.close()
+                break
+
+            elif data == "stop":
+                current_connection.shutdown(1)
+                current_connection.close()
+                exit()
+
+            elif data:
+                current_connection.send(data.encode("utf-8"))
+                print(data)
+
+
+if __name__ == "__main__":
+    try:
+        listen()
+    except KeyboardInterrupt:
+        pass
