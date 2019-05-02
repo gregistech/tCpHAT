259f94b2c33618c8f5b71dd607a792197155ab3c
thegergo02 <thegergo02@tutanota.com>
2019-05-02 14:47:40 +0200

    prototype: some test code #3

--- server.py
+++ server.py
@@ -1,30 +1,31 @@
 import socket
+from threading import Thread
+
+class Client: 
+    def __init__(self, con, name):
+        self.con = con
+        self.name = name
+
+class ListeningThread(Thread):
+    def __init__(self, connection):
+        Thread.__init__(self)
+        self.connection = connection
+
+    def run(self):
+        while True:
+            connection = self.connection
+            data = connection.recv(2048)
+            connection.send(data)
+        connection.close()
 
 def listen():
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    connection.bind(('0.0.0.0', 5555))
+    connection.bind(('0.0.0.0', 1234))
     connection.listen(10)
     while True:
-        current_connection, address = connection.accept()
-        while True:
-            data = current_connection.recv(2048)
-            data = data.decode("utf-8")
-
-            if data == "quit":
-                current_connection.shutdown(1)
-                current_connection.close()
-                break
-
-            elif data == "stop":
-                current_connection.shutdown(1)
-                current_connection.close()
-                exit()
-
-            elif data:
-                current_connection.send(data.encode("utf-8"))
-                print(data)
-
+        c, address = connection.accept()
+        curThread = ListeningThread(c)
+        curThread.start()
 
 if __name__ == "__main__":
     try:
